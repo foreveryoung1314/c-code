59db57de5e433d9c8c6d92add088985d6eb8204e
foreveryoung1314 <78845338+foreveryoung1314@users.noreply.github.com>
2021-02-10 19:31:13 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	char ch = 'A';
+	int age = 20;
+	long num = 100;
+	short df = 2;
+	long long de = 399999;
+	float  d = 3.0;
+	double a = 3.1400009;
+	printf("%c\n", ch);
+	printf("%d\n", age);
+	printf("%d\n", num);
+	printf("%d\n", df);
+	printf("%d\n", de);
+	printf("%f\n", d);
+	printf("%lf\n", a);
+	return 0;
+}
+
+
